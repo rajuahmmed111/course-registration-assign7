1bd3ee064df4124a5baa7cc9723bc387e3f7a2b6
rajuahmmed111 <ahraju2003@gmail.com>
2023-09-15 05:36:05 +0600

    third added total credit hours

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,29 @@
+import PropTypes from 'prop-types';
 
-const Cart = ({selectCourse}) => {
-    console.log(selectCourse);
+const Cart = ({ selectCourse, totalCreditHours, remaining }) => {
+    // console.log(selectCourse);
     return (
         <div>
-            <h3 className="text-2xl font-bold text-center">Course Name: {selectCourse.length}</h3>
+            <div className="mt-12">
+                <h4 className="text-xl text-[#2F80ED] font-bold">Credit Hrs Remaining: {remaining} hr</h4>
+            </div>
+            <h3 className="text-2xl font-bold text-center mt-6">Course Name: {selectCourse.length}</h3>
+
+            {
+                selectCourse.map((course) => (
+                    <li key={course.id}>{course.title}</li>
+                ))
+            }
+
+            <div className="mt-7">
+                <h4 className="text-2xl ">Total Credit Hour : {totalCreditHours}</h4>
+            </div>
         </div>
     );
 };
+
+Cart.propTypes = {
+    handleSelectCourse: PropTypes.object
+}
 
 export default Cart;
--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Cart from "../Cart/Cart";
+import PropTypes from 'prop-types';
 
 
 const Courses = () => {
     const [courses, setCourses] = useState([]);
     const [selectCourse, setSelectCourse] = useState([]);
+    const [totalCreditHours, setTotalCreditHours] = useState(0);
+
+
+    const fixedHours = 20;
 
     useEffect(() => {
         fetch("./Course.json")
@@ -15,16 +20,37 @@ const Courses = () => {
 
 
     const handleSelectCourse = (course) => {
-        setSelectCourse([...selectCourse, course]);
+        const isExist = selectCourse.find(item => item.id == course.id)
+
+        let credit = course.credit;
+
+        if (isExist) {
+            return alert("already booked");
+        }
+        else {
+            selectCourse.forEach((item) => {
+                credit = credit + item.credit;
+            });
+            const remaining = 20 - credit;
+            if (credit > 20) {
+                alert('Unavailable to credit read time')
+            }
+            else {
+                setTotalCreditHours(credit);
+                setRemaining(remaining);
+
+                setSelectCourse([...selectCourse, course]);
+            }
+        }
     }
 
 
     return (
-        <div className="container">
-            <div className="flex justify-between gap-4">
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 shadow-xl">
+        <div>
+            <div className="flex flex-col lg:flex-row justify-between gap-4 w-[1450px] border-green-300">
+                <div className="grid grid-cols-1 w-2/3 lg:grid-cols-3 gap-6 shadow-xl">
                     {courses.map((course) => (
-                        <div key={course.id} className="shadow-2xl w-80 mt-8 rounded-lg p-4">
+                        <div key={course.id} className="shadow-2xl  mt-8 rounded-lg p-4">
                             <div>
                                 <img className="w-full" src={course.img} alt="" />
                             </div>
@@ -32,11 +58,11 @@ const Courses = () => {
                             <div>
                                 <h2 className="text-2xl font-bold mt-4">{course.title}</h2>
                             </div>
-                            <p className="text-xl mt-4">{course.paragraph}</p>
+                            <p className="text-xs mt-4">{course.paragraph}</p>
 
-                            <div className="flex justify-between mt-4 mx-8">
-                                <p className="text-xl font-semibold">Price: {course.price}</p>
-                                <p className="text-xl font-semibold">Credit: {course.credit}</p>
+                            <div className="flex justify-between mt-4">
+                                <p className="text-xl">Price: {course.price}</p>
+                                <p className="text-xl">Credit: {course.credit}hr</p>
                             </div>
                             <button
                                 onClick={() => handleSelectCourse(course)}
@@ -45,12 +71,20 @@ const Courses = () => {
                         </div>
                     ))}
                 </div>
-                <div className="">
-                    <Cart selectCourse={selectCourse}></Cart>
+                <div className="w-1/3 shadow-xl h-96 rounded-xl">
+                    <Cart
+                        selectCourse={selectCourse}
+                        totalCreditHours={totalCreditHours}
+                        remaining={remaining}
+                    ></Cart>
                 </div>
             </div>
         </div>
     );
 };
+
+Courses.propTypes = {
+    handleSelectCourse: PropTypes.func
+}
 
 export default Courses;
